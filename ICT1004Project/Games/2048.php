6fe7700f30892c95a751f1c64483c7e81a4752ec
wesleychiau <38320889+wesleychiau@users.noreply.github.com>
2021-11-18 16:42:45 +0800

    Add files via upload

---
+++ ICT1004Project/Games/2048.php
@@ -0,0 +1,65 @@
+<html lang="en">
+
+    <head>
+        <?php include "head.inc.php"; ?>
+        <link href="css/2048.css" rel="stylesheet" type="text/css">
+        <script defer src="js/2048.js"></script>
+    </head>
+
+    <body>
+        <?php include "nav.inc.php"; ?>
+        <main class="container">
+            
+                <div class="heading">
+                    <h1 class="title">2048</h1>
+                    <div class="score-container">0</div>
+                </div>
+
+                <div class="game-container">
+                    <div class="game-message">
+                        <p></p>
+                        <div class="lower">
+                            <a class="retry-button">Retry</a>
+                        </div>
+                    </div>
+
+                    <div class="grid-container">
+                        <div class="grid-row">
+                            <div class="grid-cell"></div>
+                            <div class="grid-cell"></div>
+                            <div class="grid-cell"></div>
+                            <div class="grid-cell"></div>
+                        </div>
+                        <div class="grid-row">
+                            <div class="grid-cell"></div>
+                            <div class="grid-cell"></div>
+                            <div class="grid-cell"></div>
+                            <div class="grid-cell"></div>
+                        </div>
+                        <div class="grid-row">
+                            <div class="grid-cell"></div>
+                            <div class="grid-cell"></div>
+                            <div class="grid-cell"></div>
+                            <div class="grid-cell"></div>
+                        </div>
+                        <div class="grid-row">
+                            <div class="grid-cell"></div>
+                            <div class="grid-cell"></div>
+                            <div class="grid-cell"></div>
+                            <div class="grid-cell"></div>
+                        </div>
+                    </div>
+
+                    <div class="tile-container">
+
+                    </div>
+                </div>
+
+            <script defer src='https://cdnjs.cloudflare.com/ajax/libs/hammer.js/1.0.6/hammer.min.js'></script>
+
+
+        </main>
+        <?php include "footer.inc.php"; ?>
+    </body>
+</html>
+
---
+++ ICT1004Project/Games/typingtest.php
@@ -0,0 +1,33 @@
+<html lang="en">
+
+    <head>
+        <?php include "head.inc.php"; ?>
+        <link href="css/typingtest.css" rel="stylesheet" type="text/css">
+        <script defer src="js/typingtest.js"></script>
+    </head>
+
+    <body>
+        <?php include "nav.inc.php"; ?>
+        <main class="container">
+
+            <section class="test-container">
+
+                <main class="output"></main>
+                <div class="timer"></div>
+
+                <div class="word-input">
+                    <input class="type-word" type="text">
+                    <button class="refresh">↻</button>
+                    <div class="count-down">60</div>
+                </div>
+
+                <div class="result"></div>
+
+                <div class="incorrect"></div>
+            </section>  
+
+        </main>
+        <?php include "footer.inc.php"; ?>
+    </body>
+</html>
+
